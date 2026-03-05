1ebcf760e775afca33d8fc1d1a575ad9621e6ebc
JellyCondtion <aalonschakov@stud.etu.ru>
2026-03-05 15:36:29 +0300

    feat: proper no BN

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -13,11 +13,11 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=True),
+            #nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=True),
+            #nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
